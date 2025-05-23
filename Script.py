adab4b8055cbb8238303df50f37b965b592eb24e
Rocker2k <119720415+rockerz2z@users.noreply.github.com>
2025-05-24 00:10:34 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,40 +1,54 @@
 class script(object):
 
-    START_TXT = """<b>ʜᴇʏ {}, <i>{}</i>
+    START_TXT = """<b>ʜᴇʏ {},
+
+ɪ ᴀᴍ ᴛʜᴇ ᴍᴏꜱᴛ ᴘᴏᴡᴇʀꜰᴜʟ ᴀᴜᴛᴏ ꜰɪʟᴛᴇʀ ʙᴏᴛ ᴡɪᴛʜ ᴘʀᴇᴍɪᴜᴍ ꜰᴇᴀᴛᴜʀᴇꜱ, ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴇɴᴊᴏʏ
+
+<blockquote>‣ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/Rockerz2z'>Rockerz2z</a></blockquote></b>
+"""
+    MY_ABOUT_TXT = """ <blockquote><b> » Mʏ ɴᴀᴍᴇ : 
+» Dᴇᴠᴇʟᴏᴩᴇʀ : <a href='https://t.me/Rockerz2z'>R2K Kingdom</a>
+» Lɪʙʀᴀʀʏ : Pʏʀᴏɢʀᴀᴍ
+» Lᴀɴɢᴜᴀɢᴇ : Pʏᴛʜᴏɴ
+» Dᴀᴛᴀ ʙᴀsᴇ : Mᴏɴɢᴏ ᴅʙ
+» Hᴏsᴛᴇᴅ ᴏɴ  : Kᴏʏᴇʙ
+» Mʏ Vᴇʀꜱɪᴏɴ : ᴠ5.2 [sᴛᴀʙʟᴇ]</b></blockquote>"""
+
+    MY_OWNER_TXT = """<b><blockquote>⍟───[ My Details ]───⍟</blockquote>
     
-ɪ ᴀᴍ ᴘᴏᴡᴇʀғᴜʟ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ᴡɪᴛʜ ʟɪɴᴋ sʜᴏʀᴛᴇɴᴇʀ ʙᴏᴛ. ʏᴏᴜ ᴄᴀɴ ᴜꜱᴇ ᴀꜱ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ᴡɪᴛʜ ʟɪɴᴋ sʜᴏʀᴛᴇɴᴇʀ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ... ɪᴛ'ꜱ ᴇᴀꜱʏ ᴛᴏ ᴜꜱᴇ ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴀꜱ ᴀᴅᴍɪɴ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ɪ ᴡɪʟʟ ᴘʀᴏᴠɪᴅᴇ ᴛʜᴇʀᴇ ᴍᴏᴠɪᴇꜱ ᴡɪᴛʜ ʏᴏᴜʀ ʟɪɴᴋ ꜱʜᴏʀᴛᴇɴᴇʀ... ♻️</b>"""
+‣ My name: <a href=https://t.me/{}>{}</a>
+‣ My best friend: <a href='tg://settings'>This person</a> 
+‣ Developer: <a href='https://t.me/Rockerzyy_bot'>Professor R2k</a> 
+‣ Library: <a href='https://docs.pyrogram.org/'>Pyrogram</a> 
+‣ Language: <a href='https://www.python.org/download/releases/3.0/'>Python 3</a> 
+‣ Database: <a href='https://www.mongodb.com/'>Mongo DB</a> 
+‣ Bot Server: <a href='https://heroku.com'>Heroku</a> 
+‣ Build Status: v2.7.1 [BETA]</b>"""
 
-    MY_ABOUT_TXT = """★ Server: <a href=https://www.heroku.com>Heroku</a>
-★ Database: <a href=https://www.mongodb.com>MongoDB</a>
-★ Language: <a href=https://www.python.org>Python</a>
-★ Library: <a href=https://t.me/HydrogramNews>Hydrogram</a>"""
+    STATUS_TXT = """<b> » Tᴏᴛᴀʟ ᴜꜱᴇʀꜱ: <code>{}</code>
+» Pʀᴇᴍɪᴜᴍ ᴜꜱᴇʀꜱ: <code>{}</code>
+» Tᴏᴛᴀʟ ᴄʜᴀᴛꜱ: <code>{}</code>
+» Usᴇᴅ sᴛᴏʀᴀɢᴇ : <code>{}</code>
 
-    MY_OWNER_TXT = """★ Name: HA Bots
-★ Username: @Rockerz2z
-★ Country: INDIA"""
+» 𝟷ꜱᴛ Dᴀᴛᴀʙᴀꜱᴇ ғɪʟᴇꜱ: <code>{}</code>
+» 𝟸ɴᴅ Usᴇᴅ sᴛᴏʀᴀɢᴇ: <code>{}</code>
 
-    STATUS_TXT = """👤 Total Users: <code>{}</code>
-😎 Premium Users: <code>{}</code>
-👥 Total Chats: <code>{}</code>
-🗳 Data database used: <code>{}</code>
+» 𝟸ɴᴅ Dᴀᴛᴀʙᴀꜱᴇ ғɪʟᴇꜱ: <code>{}</code>
+» 𝟸ɴᴅ Dᴀᴛᴀʙᴀꜱᴇ ᴜꜱᴇᴅ: <code>{}</code>
 
-🗂 1st database Files: <code>{}</code>
-🗳 1st files database used: <code>{}</code>
-
-🗂 2nd database Files: <code>{}</code>
-🗳 2nd files database used: <code>{}</code>
-
-🚀 Bot Uptime: <code>{}</code>"""
+🚀 Bᴏᴛ ᴜᴘᴛɪᴍᴇ: <code>{}</code> </b>"""
 
     NEW_GROUP_TXT = """#NewGroup
-Title - {}
-ID - <code>{}</code>
-Username - {}
-Total - <code>{}</code>"""
+Tɪᴛʟᴇ - {}
+ɪᴅ - <code>{}</code>
+ᴜꜱᴇʀɴᴀᴍᴇ - {}
+ᴛᴏᴛᴀʟ - <code>{}</code>
+Bᴏᴛ ɴᴀᴍᴇ:- @RequestMovie_bot"""
 
     NEW_USER_TXT = """#NewUser
-★ Name: {}
-★ ID: <code>{}</code>"""
+Nᴀᴍᴇ:- {}
+ɪᴅ: <code>{}</code>
+Bᴏᴛ ɴᴀᴍᴇ:- @RequestMovie_bot"""
 
     NOT_FILE_TXT = """👋 Hello {},
 
@@ -44,6 +58,34 @@ I can't find the <b>{}</b> in my database! 🥲
 👉 Please read the Instructions to get better results.
 👉 Or not been released yet."""
     
+    EARN_TXT = """<b>ʜᴏᴡ ᴛᴏ ᴇᴀʀɴ ꜰʀᴏᴍ ᴛʜɪs ʙᴏᴛ
+
+➥ ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴀʟsᴏ ᴇᴀʀɴ ᴍᴏɴᴇʏ ʙʏ ᴜsɪɴɢ ᴛʜɪꜱ ʙᴏᴛ.
+
+» sᴛᴇᴘ 1:- ғɪʀsᴛ ʏᴏᴜ ʜᴀᴠᴇ ᴛᴏ ᴀᴅᴅ ᴛʜɪs ʙᴏᴛ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ᴀᴅᴍɪɴ ᴘᴇʀᴍɪssɪᴏɴ.
+
+» sᴛᴇᴘ 2:- ᴍᴀᴋᴇ ᴀᴄᴄᴏᴜɴᴛ ᴏɴ <a href=https://linkcents.com/ref/rockerz2ktg>linkcents.com</a> [ ʏᴏᴜ ᴄᴀɴ ᴀʟsᴏ ᴜsᴇ ᴏᴛʜᴇʀ sʜᴏʀᴛɴᴇʀ ᴡᴇʙsɪᴛᴇ ]
+
+» sᴛᴇᴘ 3:- ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴇʟᴏᴡ ɢɪᴠᴇɴ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴋɴᴏᴡ ʜᴏᴡ ᴛᴏ ᴄᴏɴɴᴇᴄᴛ ʏᴏᴜʀ sʜᴏʀᴛɴᴇʀ ᴡɪᴛʜ ᴛʜɪs ʙᴏᴛ.
+
+➥ ᴛʜɪꜱ ʙᴏᴛ ɪs ꜰʀᴇᴇ ꜰᴏʀ ᴀʟʟ, ʏᴏᴜ ᴄᴀɴ ᴜꜱᴇ ᴛʜɪꜱ ʙᴏᴛ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘs ғᴏʀ ꜰʀᴇᴇ ᴏꜰ ᴄᴏꜱᴛ.</b>"""
+
+    HOW_TXT = """<b>ʜᴏᴡ ᴛᴏ ᴄᴏɴɴᴇᴄᴛ ʏᴏᴜʀ ᴏᴡɴ sʜᴏʀᴛɴᴇʀ ‼️
+
+➥ ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴄᴏɴɴᴇᴄᴛ ʏᴏᴜʀ ᴏᴡɴ sʜᴏʀᴛɴᴇʀ ᴛʜᴇɴ ᴊᴜsᴛ sᴇɴᴅ ᴛʜᴇ ɢɪᴠᴇɴ ᴅᴇᴛᴀɪʟs ɪɴ ᴄᴏʀʀᴇᴄᴛ ꜰᴏʀᴍᴀᴛ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ
+
+➥ ғᴏʀᴍᴀᴛ ↓↓↓
+
+<code>/set_shortlink sʜᴏʀᴛɴᴇʀ sɪᴛᴇ sʜᴏʀᴛɴᴇʀ ᴀᴘɪ</code>
+
+➥ ᴇxᴀᴍᴘʟᴇ ↓↓↓
+
+<code>/set_shortlink linkcents.com 5843c3cc645f5077b2200a2c77e0344879880b3e</code>
+
+➥ ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴄʜᴇᴄᴋ ᴡʜɪᴄʜ sʜᴏʀᴛᴇɴᴇʀ ʏᴏᴜ ʜᴀᴠᴇ ᴄᴏɴɴᴇᴄᴛᴇᴅ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴛʜᴇɴ sᴇɴᴅ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴛᴏ ᴛʜᴇ ɢʀᴏᴜᴘ /get_shortlink
+
+📝 ɴᴏᴛᴇ:- ʏᴏᴜ sʜᴏᴜʟᴅ ɴᴏᴛ ʙᴇ ᴀɴ ᴀɴᴏɴʏᴍᴏᴜs ᴀᴅᴍɪɴ ɪɴ ɢʀᴏᴜᴘ. sᴇɴᴅ ᴄᴏᴍᴍᴀɴᴅ ᴡɪᴛʜᴏᴜᴛ ʙᴇɪɴɢ ᴀɴ ᴀɴᴏɴʏᴍᴜs ᴀᴅᴍɪɴ.</b>"""
+
     IMDB_TEMPLATE = """✅ I Found: <code>{query}</code>
 
 🏷 Title: <a href={url}>{title}</a>
@@ -56,21 +98,14 @@ I can't find the <b>{}</b> in my database! 🥲
 🗣 Requested by: {message.from_user.mention}
 ©️ Powered by: <b>{message.chat.title}</b>"""
 
-    FILE_CAPTION = """<i>{file_name}</i>
-
-🚫 ᴘʟᴇᴀsᴇ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ᴄʟᴏsᴇ ʙᴜᴛᴛᴏɴ ɪꜰ ʏᴏᴜ ʜᴀᴠᴇ sᴇᴇɴ ᴛʜᴇ ᴍᴏᴠɪᴇ 🚫"""
+    FILE_CAPTION = """"<b>📂Filename: {file_name}</b>\n
+<b>@Rockerz2z X @Rockerz2z</b>"""
 
     WELCOME_TEXT = """👋 Hello {mention}, Welcome to {title} group! 💞"""
 
-    HELP_TXT = """👋 Hello {},
+    HELP_TXT = """<b>Note - <blockquote>Try each command without any argument to see more details 😹</blockquote></b>"""
     
-I can filter movie and series you want
-Just type you want movie or series in my PM or adding me in to group
-And i have more feature for you
-Just try my commands"""
-
     ADMIN_COMMAND_TXT = """<b>Here is bot admin commands 👇
-
 
 /index_channels - to check how many index channel id added
 /stats - to get bot status
@@ -96,7 +131,7 @@ Just try my commands"""
 
 You can activate any premium plan and then you can get exclusive features.
 
-- INR {} for pre day -
+- INR 10 for pre day -
 
 Basic premium features:
 Ad free experience
@@ -107,7 +142,8 @@ No shortlink
 Admins support
 And more...
 
-Support: {}"""
+Support:
+@Rockerz2z"""
 
     USER_COMMAND_TXT = """<b>Here is bot user commands 👇
 
@@ -123,6 +159,6 @@ Support: {}"""
 
 - ᴛʜɪꜱ ʙᴏᴛ ɪꜱ ᴀɴ ᴏᴘᴇɴ ꜱᴏᴜʀᴄᴇ ᴘʀᴏᴊᴇᴄᴛ.
 
-- ꜱᴏᴜʀᴄᴇ - <a href=https://t.me/Rockerz2z>ʜᴇʀᴇ</a>
+- ꜱᴏᴜʀᴄᴇ - <a href='https://t.me/Rockerz2z'>R2K Kingdom</a>
 
-- ᴅᴇᴠʟᴏᴘᴇʀ - @Rockerz2z"""
+- ᴅᴇᴠʟᴏᴘᴇʀ - @Rockerz2z</b>"""
