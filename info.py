eb96887a79b52d294e2405a8543722d5d93432a4
Rocker2k <119720415+rockerz2z@users.noreply.github.com>
2025-05-24 17:43:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ else:
     ADMINS = [int(admins) for admins in ADMINS.split()]
 
 # Channels
-INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '-1001660466640 -1001505006734 -1001698270642').split()]
+INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '-1001660466640 -1001505006734').split()]
 if len(INDEX_CHANNELS) == 0:
     logger.info('INDEX_CHANNELS is empty')
 LOG_CHANNEL = environ.get('LOG_CHANNEL', '-1002280301241')
